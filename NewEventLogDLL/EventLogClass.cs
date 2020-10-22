3313a99f3b967a1f263aa3f0119edf19fa5313f0
Terry Holmes <tchaser3@outlook.com>
2020-10-22 12:10:42 -0400

    Add project files.

---
+++ NewEventLogDLL/EventLogClass.cs
@@ -0,0 +1,185 @@
+﻿/* Title:           New Event Log DLL
+ * Date:            4-24-17
+ * Author:          Terry Holmes
+ * 
+ * Description:     This Class is for the new Event Log */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace NewEventLogDLL
+{
+    public class EventLogClass
+    {
+        //setting up the data
+        EventLogDataSet aEventLogDataSet;
+        EventLogDataSetTableAdapters.eventlogTableAdapter aEventLogTableAdapter;
+
+        InsertEventLogEntry aInsertEventLogEntry;
+        InsertEventLogEntryTableAdapters.QueriesTableAdapter aInsertEventLogTableAdapter;
+
+        FindEventLogByDateRangeDataSet aFindEventLogByDateRangeDataSet;
+        FindEventLogByDateRangeDataSetTableAdapters.FindEventLogEntriesByDateRangeTableAdapter aFindEventLogByDateRangeTableAdapter;
+
+        ServerEventLogDataSet aServerEventLogDataSet;
+        ServerEventLogDataSetTableAdapters.servereventlogTableAdapter aServerEventLogTableAdapter;
+
+        InsertServerEventLogEntryTableAdapters.QueriesTableAdapter aInsertServerEventLogEntryTableAdapter;
+
+        FindServerEventLogByTransactionDateDataSet aFindServerEventLogByTransactionDateDataSet;
+        FindServerEventLogByTransactionDateDataSetTableAdapters.FindServerEventLogByTransactionDateTableAdapter aFindServerEventLogByTransactionDateTableAdapter;
+
+        FindServerEventLogByNoteKeywordDataSet aFindServerEventLogByNoteKeywordDataSet;
+        FindServerEventLogByNoteKeywordDataSetTableAdapters.FindServerLogByNoteKeywordTableAdapter aFindServerEVentLogByNoteKeywordTableAdapter;
+
+        public FindServerEventLogByNoteKeywordDataSet FindServerEventLogByNoteKeyword(string strNoteKeyword, DateTime datStartDate, DateTime datEndDate)
+        {
+            try
+            {
+                aFindServerEventLogByNoteKeywordDataSet = new FindServerEventLogByNoteKeywordDataSet();
+                aFindServerEVentLogByNoteKeywordTableAdapter = new FindServerEventLogByNoteKeywordDataSetTableAdapters.FindServerLogByNoteKeywordTableAdapter();
+                aFindServerEVentLogByNoteKeywordTableAdapter.Fill(aFindServerEventLogByNoteKeywordDataSet.FindServerLogByNoteKeyword, strNoteKeyword, datStartDate, datEndDate);
+            }
+            catch (Exception Ex)
+            {
+                InsertEventLogEntry(DateTime.Now, "Event Log Class // Find Server Event Log By Note Keyword " + Ex.Message);
+            }
+
+            return aFindServerEventLogByNoteKeywordDataSet;
+        }
+        public FindServerEventLogByTransactionDateDataSet FindServerEventLogByTransactionDate(DateTime datTransactionDate)
+        {
+            try
+            {
+                aFindServerEventLogByTransactionDateDataSet = new FindServerEventLogByTransactionDateDataSet();
+                aFindServerEventLogByTransactionDateTableAdapter = new FindServerEventLogByTransactionDateDataSetTableAdapters.FindServerEventLogByTransactionDateTableAdapter();
+                aFindServerEventLogByTransactionDateTableAdapter.Fill(aFindServerEventLogByTransactionDateDataSet.FindServerEventLogByTransactionDate, datTransactionDate);
+            }
+            catch (Exception Ex)
+            {
+                InsertEventLogEntry(DateTime.Now, "Event Log Class // Find Server Event Log By Transaction Date " + Ex.Message);
+            }
+
+            return aFindServerEventLogByTransactionDateDataSet;
+        }
+        public bool InsertServerEventLog(DateTime datTransactionDate, string strEventCategory, string strEventNotes)
+        {
+            bool blnFatalError = false;
+
+            try
+            {
+                aInsertServerEventLogEntryTableAdapter = new InsertServerEventLogEntryTableAdapters.QueriesTableAdapter();
+                aInsertServerEventLogEntryTableAdapter.InsertServerEventLog(datTransactionDate, strEventCategory, strEventNotes, DateTime.Now);
+            }
+            catch (Exception Ex)
+            {
+                InsertEventLogEntry(DateTime.Now, "Event Log Class // inserst Server Event Log " + Ex.Message);
+
+                blnFatalError = true;
+            }
+
+            return blnFatalError;
+        }
+        public ServerEventLogDataSet GetServerEventLogInfo()
+        {
+            try
+            {
+                aServerEventLogDataSet = new ServerEventLogDataSet();
+                aServerEventLogTableAdapter = new ServerEventLogDataSetTableAdapters.servereventlogTableAdapter();
+                aServerEventLogTableAdapter.Fill(aServerEventLogDataSet.servereventlog);
+            }
+            catch (Exception Ex)
+            {
+                InsertEventLogEntry(DateTime.Now, "Event Log Class // Get Server Event Log Info " + Ex.Message);
+            }
+
+            return aServerEventLogDataSet;
+        }
+        public void UpdateServerEventLogDB(ServerEventLogDataSet aServerEventLogDataSet)
+        {
+            try
+            {
+                aServerEventLogTableAdapter = new ServerEventLogDataSetTableAdapters.servereventlogTableAdapter();
+                aServerEventLogTableAdapter.Update(aServerEventLogDataSet.servereventlog);
+            }
+            catch (Exception Ex)
+            {
+                InsertEventLogEntry(DateTime.Now, "Event Log Class // Update Server Event Log DB " + Ex.Message);
+            }
+        }
+        public FindEventLogByDateRangeDataSet FindEventLogByDateRange(DateTime datStartDate, DateTime datEndDate)
+        {
+            try
+            {
+                aFindEventLogByDateRangeDataSet = new FindEventLogByDateRangeDataSet();
+                aFindEventLogByDateRangeTableAdapter = new FindEventLogByDateRangeDataSetTableAdapters.FindEventLogEntriesByDateRangeTableAdapter();
+                aFindEventLogByDateRangeTableAdapter.Fill(aFindEventLogByDateRangeDataSet.FindEventLogEntriesByDateRange, datStartDate, datEndDate);
+            }
+            catch (Exception)
+            {
+                
+            }
+
+            return aFindEventLogByDateRangeDataSet;
+        }
+        public bool InsertEventLogEntry(DateTime datEventDate, string strEventLogEntry)
+        {
+            bool blnFatalError = false;
+            string strComputerName;
+            string strUser;
+
+            try
+            {
+                strComputerName = System.Environment.MachineName;
+                strUser = System.Environment.UserName;
+
+                strEventLogEntry = strUser + " " + strComputerName + " " + strEventLogEntry;
+
+                aInsertEventLogEntry = new InsertEventLogEntry();
+                aInsertEventLogTableAdapter = new InsertEventLogEntryTableAdapters.QueriesTableAdapter();
+                aInsertEventLogTableAdapter.InsertEventLogEntry(datEventDate, strEventLogEntry);
+
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return blnFatalError;
+        }
+        public EventLogDataSet GetEventLogInfo()
+        {
+            try
+            {
+                aEventLogDataSet = new EventLogDataSet();
+                aEventLogTableAdapter = new EventLogDataSetTableAdapters.eventlogTableAdapter();
+                aEventLogTableAdapter.Fill(aEventLogDataSet.eventlog);
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return aEventLogDataSet;
+        }
+        public void UpdateEventLogDB(EventLogDataSet aEventLogDataSet)
+        {
+            try
+            {
+                aEventLogTableAdapter = new EventLogDataSetTableAdapters.eventlogTableAdapter();
+                aEventLogTableAdapter.Update(aEventLogDataSet.eventlog);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        
+    }
+}
